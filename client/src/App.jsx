2cf0851755136043151e2a66095ccbefcc91b029
VarunPareek <114807315+varunpareek690@users.noreply.github.com>
2024-03-15 00:47:33 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Navbar,Transactions,Services,Welcome,Loader,Footer } from './components'
+
+const App = () => {
+  return (
+    <div className = "min-h-screen">
+      <div className='gradient-bg-welcome'>
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
