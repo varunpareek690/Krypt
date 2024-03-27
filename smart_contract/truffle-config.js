0ac3f1c45e50d6a45679ff57c2d63bc359cb24ec
Varun Pareek <114807315+varunpareek690@users.noreply.github.com>
2024-03-28 02:25:43 +0530

    Changes in truffle-config.js

--- smart_contract/truffle-config.js
+++ smart_contract/truffle-config.js
@@ -1,23 +1,23 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 // Add your Sepolia network private key and RPC URL
-const sepoliaPrivateKey = "1a4921b239018b2a59dcbaf3de8e34e2e9c7e23d39b3ac97109c1cf481f5ce48";
-const sepoliaRpcUrl = "wss://sepolia.infura.io/ws/v3/59aff5b12dea44bd8f2dd146f3db59db";
+const sepoliaPrivateKey = "add your private key";
+const sepoliaRpcUrl = "add a node from infura/alchemy or local server such as ganache";
 
 module.exports = {
   networks: {
     sepolia: {
       provider: () => new HDWalletProvider(sepoliaPrivateKey, sepoliaRpcUrl),
-      network_id: "11155111", // Match any network id
-      gas: 5500000, // Adjust the gas limit as needed
-      gasPrice: 10000000000 // Adjust the gas price as needed
+      network_id: "11155111", 
+      gas: 5500000, 
+      gasPrice: 10000000000 
     }
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.19",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.19",  
     },
   },
 };
